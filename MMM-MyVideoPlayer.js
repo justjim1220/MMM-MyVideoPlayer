524550cfb3e52c9f776b59deacc08ecc1ed78823
JIM HALLOCK <justjim1220@gmail.com>
2018-12-11 20:29:24 -0600

    Update MMM-MyVideoPlayer.js

--- MMM-MyVideoPlayer.js
+++ MMM-MyVideoPlayer.js
@@ -158,35 +158,3 @@ Module.register("MMM-MyVideoPlayer", {
 		return item;
 	}
 });
-
-
-/*
-
- <div class="videolist">
-	<nav class="vids">
-		<a class="link" href="https://www.quirksmode.org/html5/videos/big_buck_bunny.mp4">test1</a>
-
-		<a class="link" href="https://www.quirksmode.org/html5/videos/big_buck_bunny.mp4">test2</a>
-
-		<a class="link" href="http://www.html5videoplayer.net/videos/toystory.mp4">test3</a>
-
-		<a class="link" href="http://download.wavetlan.com/SVV/Media/HTTP/H264/Talkinghead_Media/H264_test4_Talkingheadclipped_mp4_480x320.mp4">test4</a>
-
-		<a class="link" href="http://download.wavetlan.com/SVV/Media/HTTP/H264/Other_Media/H264_test5_voice_mp4_480x360.mp4">test5</a>
-
-		<a class="link" href="http://download.wavetlan.com/SVV/Media/HTTP/H264/Other_Media/H264_test7_voiceclip_mp4_480x360.mp4">test6</a>
-
-		<a class="link" href="http://download.wavetlan.com/SVV/Media/HTTP/H264/Other_Media/H264_test8_voiceclip_mp4_480x320.mp4">test7</a>
-
-		<a class="link" href="http://download.wavetlan.com/SVV/Media/HTTP/MP4/ConvertedFiles/Media-Convert/Unsupported/dw11222.mp4">test8</a>
-
-		<a class="link" href="http://download.wavetlan.com/SVV/Media/HTTP/MP4/ConvertedFiles/Media-Convert/Unsupported/test7.mp4">tsest9</a>
-	</nav>
-
-
-if (buttons !== "") {
-	for (var i = 0, len = videos.length; i < len; i++) {
-	  }
-	}
-
-*/
