5159ac8e0284760be48ee7d2aa9a51e02537647f
JIM HALLOCK <justjim1220@gmail.com>
2018-12-12 20:29:31 -0600

    Add files via upload

--- MMM-MyVideoPlayer.js
+++ MMM-MyVideoPlayer.js
@@ -1,7 +1,8 @@
 /* Magic Mirror
  * Module: MMM-VideoPlayer
  *
- * By justjim1220
+ * By justjim1220 (Jim Hallock)
+ * justjim1220@gmail.com
  *
  * MIT Licensed.
  *
@@ -13,30 +14,10 @@ Module.register("MMM-MyVideoPlayer", {
 	// Default module config.
 	defaults: {
 		initialLoadDelay: 5150,
-
-		videos: ["video_one", "Rx_by_Theory", "three", "fourth_video", "Hallelujah"],
-
 		showBorder: true,
 		minWidth: "212px",
 		minHeight: "50px",
 		direction: "row",
-		buttons: {
-			"video_one": {
-				text: "Video 1"
-			},
-			"Rx_by_Theory": {
-				text: "Video 2"
-			},
-			"three": {
-				text: "Video 3"
-			},
-			"fourth_video": {
-				text: "Video 4"
-			},
-			"Hallelujah": {
-				text: "Video 5"
-			}
-		}
 	},
 
 	requiresversion: "2.1.0",
@@ -53,64 +34,41 @@ Module.register("MMM-MyVideoPlayer", {
 	},
 
 	// Override dom generator.
-	getDom: function () {
+	getDom: function() {
 		var wrapper = document.createElement("div");
 
-		var videos = this.config.videos;
-		videos = videos[Math.floor(Math.random() * videos.length)];
+		wrapper.innerHTML = `<video class='controls' height='675' width='1200' id="player"></video>
+			<div id="videoSelect">
+				<button data-video-src="file:///modules/MMM-MyVideoPlayer/videos/five.mp4" class="button">Video 1</button>
+				<button data-video-src="modules/MMM-MyVideoPlayer/videos/two.mp4" class="button">Video 2</button>
+				<button data-video-src="modules/MMM-MyVideoPlayer/videos/EverLast - what it's like (EXPLICIT).mp4" class="button">Video 3</button>
+				<button data-video-src="modules/MMM-MyVideoPlayer/videos/Tesla-What You Give.mp4" class="button">Video 4</button>
+				<button data-video-src="modules/MMM-MyVideoPlayer/videos/I_Apologize.mp4" class="button">Video 5</button>
+				<button data-video-src="modules/MMM-MyVideoPlayer/videos/Rx_by_Theory.mp4" class="button">Video 6</button>
+				<button data-video-src="modules/MMM-MyVideoPlayer/videos/ten.mp4" class="button">Video 7</button>
+				<button data-video-src="modules/MMM-MyVideoPlayer/videos/Hallelujah.mp4.mp4" class="button">Video 8</button>
+			</div>`;
 
-		if (videos == "") {
-			wrapper.classList.add("font");
-			wrapper.innerHTML = "Please add videos to your config array as explained in Readme";
-		} else if (videos != "") {
-			wrapper.innerHTML = `<video autoplay controls height='600' width='1066' id="video"><source src="modules/MMM-MyVideoPlayer/videos/${videos}.mp4" type="video/mp4"></video>`;
-		}
 		console.log(wrapper.innerHTML);
+
+		function swapVideo() {
+			player.src = this.getAttribute("data-video-src");
+			player.load();
+			player.play();
+		}
+
+		var videoPlayButtons = document.querySelectorAll("button"),
+			player = document.getElementById("player");
+
+		for (var i = 0; i < videoPlayButtons.length; i++) {
+			videoPlayButtons[i].addEventListener("click", swapVideo);
+		}
 
 		var menu = document.createElement("span");
 		menu.className = "navMenu";
 		menu.id = this.identifier + "_menu";
 		menu.style.flexDirection = this.config.direction;
 
-		for (var name in this.config.buttons) {
-			menu.appendChild(this.createButton(this, name, this.config.buttons[name]));
-		}
-		wrapper.appendChild(menu);
-
 		return wrapper;
 	},
-
-	createButton: function (self, name, data) {
-		var item = document.createElement("span");
-		item.id = self.identifier + "_button_" + name;
-		item.className = "navBtn";
-		item.style.minWidth = self.config.minWidth;
-		item.style.minHeight = self.config.minHeight;
-		item.style.flexDirection = self.config.direction;
-
-		item.addEventListener("play", function () {
-			item.play();
-		}, false);
-
-		if (self.selected === name) {
-			item.onclick = function () {
-				item.play();
-				item.pause();
-				item.controls = "controls";
-			}
-		}
-
-		if (!self.config.showBorder) {
-			item.style.borderColor = "black";
-		}
-
-		if (data.text) {
-			var text = document.createElement("span");
-			text.className = "navText";
-			text.innerHTML = data.text;
-
-			item.appendChild(text);
-		}
-		return item;
-	}
 });
